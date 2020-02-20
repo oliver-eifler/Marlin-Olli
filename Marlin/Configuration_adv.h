06f07a492bdeedab85dabbe5e925e1064aec8413
Oliver Jean Eifler <olli.eifler@gmail.com>
2020-02-20 01:19:06 +0100

    configuration update

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2051,15 +2051,17 @@
   #endif
 
   #if AXIS_IS_TMC(Z)
-    #define Z_CURRENT     1000
-    #define Z_MICROSTEPS   4
+    #define Z_CURRENT     800
+    #define Z_CURRENT_HOME Z_CURRENT
+    #define Z_MICROSTEPS   16
     #define Z_RSENSE     0.11
     #define Z_CHAIN_POS    -1
   #endif
 
   #if AXIS_IS_TMC(Z2)
-    #define Z2_CURRENT    1000
-    #define Z2_MICROSTEPS  4
+    #define Z2_CURRENT    800
+    #define Z2_CURRENT_HOME Z2_CURRENT
+    #define Z2_MICROSTEPS  16
     #define Z2_RSENSE    0.11
     #define Z2_CHAIN_POS   -1
   #endif
