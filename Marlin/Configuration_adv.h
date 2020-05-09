2baa9cd1cd93a8ba4877938ea2ae231e710a38dc
Oliver Jean Eifler <olli.eifler@gmail.com>
2020-05-09 16:33:17 +0200

    update config

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2126,11 +2126,11 @@
  */
 #if HAS_TRINAMIC_CONFIG
 
-  #define HOLD_MULTIPLIER    0.5  // Scales down the holding current from run current
+  #define HOLD_MULTIPLIER    0.6  // Scales down the holding current from run current
   #define INTERPOLATE       true  // Interpolate X/Y/Z_MICROSTEPS to 256
 
   #if AXIS_IS_TMC(X)
-    #define X_CURRENT       950        // (mA) RMS current. Multiply by 1.414 for peak current.
+    #define X_CURRENT       800        // (mA) RMS current. Multiply by 1.414 for peak current.
     #define X_CURRENT_HOME  X_CURRENT  // (mA) RMS current for sensorless homing
     #define X_MICROSTEPS     16    // 0..256
     #define X_RSENSE          0.11
@@ -2146,7 +2146,7 @@
   #endif
 
   #if AXIS_IS_TMC(Y)
-    #define Y_CURRENT       950
+    #define Y_CURRENT       800
     #define Y_CURRENT_HOME  Y_CURRENT
     #define Y_MICROSTEPS     16
     #define Y_RSENSE          0.11
@@ -2162,7 +2162,7 @@
   #endif
 
   #if AXIS_IS_TMC(Z)
-    #define Z_CURRENT     950
+    #define Z_CURRENT     800
     #define Z_CURRENT_HOME Z_CURRENT
     #define Z_MICROSTEPS   16
     #define Z_RSENSE     0.11
@@ -2170,7 +2170,7 @@
   #endif
 
   #if AXIS_IS_TMC(Z2)
-    #define Z2_CURRENT    950
+    #define Z2_CURRENT    800
     #define Z2_CURRENT_HOME Z2_CURRENT
     #define Z2_MICROSTEPS  16
     #define Z2_RSENSE    0.11
@@ -2194,7 +2194,7 @@
   #endif
 
   #if AXIS_IS_TMC(E0)
-    #define E0_CURRENT      950
+    #define E0_CURRENT      800
     #define E0_MICROSTEPS    16
     #define E0_RSENSE         0.11
     #define E0_CHAIN_POS     -1
