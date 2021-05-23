3ddeedb1ed1fa731b50268e6e3a37414b798dbac
Oliver Jean Eifler <olli.eifler@gmail.com>
2021-05-23 04:14:54 +0200

    update olli skr pro board

--- Marlin/src/pins/olli/pins_OLLI_SKR_PRO_V1_1.h
+++ Marlin/src/pins/olli/pins_OLLI_SKR_PRO_V1_1.h
@@ -32,18 +32,22 @@
 
 #define BOARD_INFO_NAME "OLLI BTT SKR Pro 1.1" // redefined?
 
-// BigTreeTech driver expansion module https://bit.ly/3ptRRoj
+// If you have the BigTreeTech driver expansion module, enable BTT_MOTOR_EXPANSION
+// https://github.com/bigtreetech/BTT-Expansion-module/tree/master/BTT%20EXP-MOT
 //#define BTT_MOTOR_EXPANSION
 
 #if BOTH(HAS_WIRED_LCD, BTT_MOTOR_EXPANSION)
-  #error "It's not possible to have both LCD and motor expansion module on EXP1/EXP2."
+  #if EITHER(CR10_STOCKDISPLAY, ENDER2_STOCKDISPLAY)
+    #define EXP_MOT_USE_EXP2_ONLY 1
+  #else
+    #error "You can't use both an LCD and a Motor Expansion Module on EXP1/EXP2 at the same time."
+  #endif
 #endif
 
 // Use one of these or SDCard-based Emulation will be used
 #if NO_EEPROM_SELECTED
   //#define SRAM_EEPROM_EMULATION                 // Use BackSRAM-based EEPROM emulation
-  //#define FLASH_EEPROM_EMULATION                // Use Flash-based EEPROM emulation
-  #define I2C_EEPROM                              // Use excternal I2C EEPROM
+  #define FLASH_EEPROM_EMULATION                  // Use Flash-based EEPROM emulation
 #endif
 
 #if ENABLED(FLASH_EEPROM_EMULATION)
@@ -67,16 +71,16 @@
 #define X_DIAG_PIN                          PB10  // X-
 #define Y_DIAG_PIN                          PE12  // Y-
 #define Z_DIAG_PIN                          PG8   // Z-
-#define Z2_DIAG_PIN                         PE15  // E0
-#define E0_DIAG_PIN                         PE10  // E1
-#define E1_DIAG_PIN                         PG5   // E2
+#define E0_DIAG_PIN                         PE15  // E0
+#define E1_DIAG_PIN                         PE10  // E1
+#define E2_DIAG_PIN                         PG5   // E2
 
 //
 // Limit Switches
 //
 #ifdef X_STALL_SENSITIVITY
   #define X_STOP_PIN                  X_DIAG_PIN
-  #if X_HOME_DIR < 0
+  #if X_HOME_TO_MIN
     #define X_MAX_PIN                       PE15  // E0
   #else
     #define X_MIN_PIN                       PE15  // E0
@@ -88,7 +92,7 @@
 
 #ifdef Y_STALL_SENSITIVITY
   #define Y_STOP_PIN                  Y_DIAG_PIN
-  #if Y_HOME_DIR < 0
+  #if Y_HOME_TO_MIN
     #define Y_MAX_PIN                       PE10  // E1
   #else
     #define Y_MIN_PIN                       PE10  // E1
@@ -100,7 +104,7 @@
 
 #ifdef Z_STALL_SENSITIVITY
   #define Z_STOP_PIN                  Z_DIAG_PIN
-  #if Z_HOME_DIR < 0
+  #if Z_HOME_TO_MIN
     #define Z_MAX_PIN                       PG5   // E2
   #else
     #define Z_MIN_PIN                       PG5   // E2
@@ -143,7 +147,7 @@
 #define Y_STEP_PIN                          PE11
 #define Y_DIR_PIN                           PE8
 #define Y_ENABLE_PIN                        PD7
- #ifndef Y_CS_PIN
+#ifndef Y_CS_PIN
   #define Y_CS_PIN                          PB8
 #endif
 
@@ -154,25 +158,25 @@
   #define Z_CS_PIN                          PB9
 #endif
 
-#define Z2_STEP_PIN                         PE14
-#define Z2_DIR_PIN                          PA0
-#define Z2_ENABLE_PIN                       PC3
-#ifndef Z2_CS_PIN
-  #define Z2_CS_PIN                         PB3
-#endif
-
-#define E0_STEP_PIN                         PD15
-#define E0_DIR_PIN                          PE7
-#define E0_ENABLE_PIN                       PA3
+#define E0_STEP_PIN                         PE14
+#define E0_DIR_PIN                          PA0
+#define E0_ENABLE_PIN                       PC3
 #ifndef E0_CS_PIN
-  #define E0_CS_PIN                         PG15
+  #define E0_CS_PIN                         PB3
 #endif
 
-#define E1_STEP_PIN                         PD13
-#define E1_DIR_PIN                          PG9
-#define E1_ENABLE_PIN                       PF0
+#define E1_STEP_PIN                         PD15
+#define E1_DIR_PIN                          PE7
+#define E1_ENABLE_PIN                       PA3
 #ifndef E1_CS_PIN
-  #define E1_CS_PIN                         PG12
+  #define E1_CS_PIN                         PG15
+#endif
+
+#define E2_STEP_PIN                         PD13
+#define E2_DIR_PIN                          PG9
+#define E2_ENABLE_PIN                       PF0
+#ifndef E2_CS_PIN
+  #define E2_CS_PIN                         PG12
 #endif
 
 //
@@ -221,14 +225,14 @@
   #define Z_SERIAL_TX_PIN                   PE1
   #define Z_SERIAL_RX_PIN                   PE1
 
-  #define Z2_SERIAL_TX_PIN                  PD4
-  #define Z2_SERIAL_RX_PIN                  PD4
+  #define E0_SERIAL_TX_PIN                  PD4
+  #define E0_SERIAL_RX_PIN                  PD4
 
-  #define E0_SERIAL_TX_PIN                  PD1
-  #define E0_SERIAL_RX_PIN                  PD1
+  #define E1_SERIAL_TX_PIN                  PD1
+  #define E1_SERIAL_RX_PIN                  PD1
 
-  #define E1_SERIAL_TX_PIN                  PD6
-  #define E1_SERIAL_RX_PIN                  PD6
+  #define E2_SERIAL_TX_PIN                  PD6
+  #define E2_SERIAL_RX_PIN                  PD6
 
   // Reduce baud rate to improve software serial reliability
   #define TMC_BAUD_RATE                    19200
@@ -321,48 +325,59 @@
 #endif
 
 #if ENABLED(BTT_MOTOR_EXPANSION)
-  /**
-   *               _____                                      _____
-   *           NC | · · | GND                             NC | · · | GND
-   *           NC | · · | PF12 (M1EN)            (M2EN)  PG7 | · · | PG6  (M3EN)
-   * (M1STP) PB15 | · ·   PF11 (M1DIR)           (M1RX)  PG3 | · ·   PG2  (M1DIAG)
-   * (M2DIR) PB12 | · · | PG10 (M2STP)           (M2RX) PD10 | · · | PD11 (M2DIAG)
-   * (M3DIR) PB13 | · · | PB14 (M3STP)           (M3RX)  PA8 | · · | PG4  (M3DIAG)
-   *               -----                                      -----
-   *               EXP2                                       EXP1
+  /**       _____                        _____
+   *    NC | . . | GND               NC | . . | GND
+   *    NC | . . | M1EN            M2EN | . . | M3EN
+   * M1STP | . .   M1DIR           M1RX | . .   M1DIAG
+   * M2DIR | . . | M2STP           M2RX | . . | M2DIAG
+   * M3DIR | . . | M3STP           M3RX | . . | M3DIAG
+   *        -----                        -----
+   *        EXP2                         EXP1
+   *
+   * NB In EXP_MOT_USE_EXP2_ONLY mode EXP1 is not used and M2EN and M3EN need to be jumpered to M1EN
    */
 
   // M1 on Driver Expansion Module
-  #define E2_STEP_PIN                EXP2_05_PIN
-  #define E2_DIR_PIN                 EXP2_06_PIN
-  #define E2_ENABLE_PIN              EXP2_04_PIN
-  #define E2_DIAG_PIN                EXP1_06_PIN
-  #define E2_CS_PIN                  EXP1_05_PIN
-  #if HAS_TMC_UART
-    #define E2_SERIAL_TX_PIN         EXP1_05_PIN
-    #define E2_SERIAL_RX_PIN         EXP1_05_PIN
+  #define E3_STEP_PIN                EXP2_05_PIN
+  #define E3_DIR_PIN                 EXP2_06_PIN
+  #define E3_ENABLE_PIN              EXP2_04_PIN
+  #if !EXP_MOT_USE_EXP2_ONLY
+    #define E3_DIAG_PIN              EXP1_06_PIN
+    #define E3_CS_PIN                EXP1_05_PIN
+    #if HAS_TMC_UART
+      #define E3_SERIAL_TX_PIN       EXP1_05_PIN
+      #define E3_SERIAL_RX_PIN       EXP1_05_PIN
+    #endif
   #endif
 
   // M2 on Driver Expansion Module
-  #define E3_STEP_PIN                EXP2_08_PIN
-  #define E3_DIR_PIN                 EXP2_07_PIN
-  #define E3_ENABLE_PIN              EXP1_03_PIN
-  #define E3_DIAG_PIN                EXP1_08_PIN
-  #define E3_CS_PIN                  EXP1_07_PIN
-  #if HAS_TMC_UART
-    #define E3_SERIAL_TX_PIN         EXP1_07_PIN
-    #define E3_SERIAL_RX_PIN         EXP1_07_PIN
+  #define E4_STEP_PIN                EXP2_08_PIN
+  #define E4_DIR_PIN                 EXP2_07_PIN
+  #if !EXP_MOT_USE_EXP2_ONLY
+    #define E4_ENABLE_PIN            EXP1_03_PIN
+    #define E4_DIAG_PIN              EXP1_08_PIN
+    #define E4_CS_PIN                EXP1_07_PIN
+    #if HAS_TMC_UART
+      #define E4_SERIAL_TX_PIN       EXP1_07_PIN
+      #define E4_SERIAL_RX_PIN       EXP1_07_PIN
+    #endif
+  #else
+    #define E4_ENABLE_PIN            EXP2_04_PIN
   #endif
 
   // M3 on Driver Expansion Module
-  #define E4_STEP_PIN                EXP2_10_PIN
-  #define E4_DIR_PIN                 EXP2_09_PIN
-  #define E4_ENABLE_PIN              EXP1_04_PIN
-  #define E4_DIAG_PIN                EXP1_10_PIN
-  #define E4_CS_PIN                  EXP1_09_PIN
-  #if HAS_TMC_UART
-    #define E4_SERIAL_TX_PIN         EXP1_09_PIN
-    #define E4_SERIAL_RX_PIN         EXP1_09_PIN
+  #define E5_STEP_PIN                EXP2_10_PIN
+  #define E5_DIR_PIN                 EXP2_09_PIN
+  #if !EXP_MOT_USE_EXP2_ONLY
+    #define E5_ENABLE_PIN            EXP1_04_PIN
+    #define E5_DIAG_PIN              EXP1_10_PIN
+    #define E5_CS_PIN                EXP1_09_PIN
+    #if HAS_TMC_UART
+      #define E5_SERIAL_TX_PIN       EXP1_09_PIN
+      #define E5_SERIAL_RX_PIN       EXP1_09_PIN
+    #endif
+  #else
+    #define E5_ENABLE_PIN            EXP2_04_PIN
   #endif
 
 #endif // BTT_MOTOR_EXPANSION
